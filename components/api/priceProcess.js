7f18245cb76aa7dd048ab0f809d1f0a7d376370d
joshua-yan <joshuayan99@gmail.com>
2019-08-08 16:44:55 -0400

    fixed next build fail

--- components/api/priceProcess.js
+++ components/api/priceProcess.js
@@ -1,37 +1,7 @@
-import $ from 'jQuery';
 import axios from "axios";
-async function priceProcess(ingredientList)
-{
+
+async function priceProcess(ingredientList) {
     let spoonKey = "da70221b4bfd4192922aa75c3f306071";
-    let data = {
-        "ingredients": ingredientList,
-        "servings": 1
-    }
-    // $.ajax({
-    //     type: "POST",
-    //     contentType: "application/json",
-    //     url: "https://api.spoonacular.com/food/ingredients/map",
-    //     data: JSON.stringify(data),
-    //     dataType: "json",
-    //     success: function(idResponse) {
-    //         for (let ingredient of idResponse)
-    //         {
-    //             // let id = ingredient.products[0].id;
-    //             // let name = ingredient.original;
-    //             // let price = null;
-    //             $.ajax({
-    //                 type: "GET",
-    //                 contentType: "application/json",
-    //                 url: "https://api.spoonacular.com/food/products/" + ingredient["products"][0]["id"],
-    //                 dataType: "json",
-    //                 success: function(productResponse) {
-    //                     //name and price into array
-    //                     priceEstimates.push([ingredient["original"], productResponse["price"]]);
-    //                 }
-    //             });
-    //         }
-    //     }
-    // });
     let idResponse = (await axios.post("https://api.spoonacular.com/food/ingredients/map?apiKey=" + spoonKey, {
         "ingredients": ingredientList,
         "servings": 1
