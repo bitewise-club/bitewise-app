a2842b555e7887e4b527216e8ffe588b6c1621d4
joshua-yan <joshuayan99@gmail.com>
2019-08-08 11:48:49 -0400

    added prompt for camera input

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,49 @@
 import React from 'react';
 import {createMuiTheme} from '@material-ui/core/styles';
 import {ThemeProvider} from '@material-ui/styles';
+import {ButtonBase, Divider, Grid, makeStyles, Typography} from '@material-ui/core';
+import CameraPrompt from "../components/CameraPrompt";
 import '../static/default.css';
 
 const theme = createMuiTheme({
-    palette:{
-        primary:{
+    palette: {
+        primary: {
             main: "#0096db",
         },
-        secondary:{
+        secondary: {
             main: "#29c609",
         },
     },
-    typography:{
+    typography: {
         fontFamily: [
+            'Avenir',
             'Arial',
         ].join(','),
     },
 });
 
-class Index extends React.Component{
+const useStyles = makeStyles(theme => ({
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+}));
 
-    host = process.env.API_HOST;
-
+class Index extends React.Component {
     render() {
         return (
             <ThemeProvider theme={theme}>
-                <h1>{this.host}</h1>
+                <div>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} alignContent="center">
+                            <Typography variant="h1" align="center" gutterBottom>
+                                BiteWise
+                            </Typography>
+                            <Divider/>
+                            <CameraPrompt/>
+                        </Grid>
+                    </Grid>
+                </div>
             </ThemeProvider>
         );
     }
